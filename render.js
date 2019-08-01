a08f1cf5f5dc73389d2a3008024a74c703180d17
Yiannis Despoti <despotis@ucm.org.cy>
2019-08-01 13:03:48 +0300

    svg test

--- render.js
+++ render.js
@@ -3,12 +3,24 @@ var map,
     kmlDataSources = {
         nicosia: 'https://raw.githubusercontent.com/yiannisdesp/ucmmap/master/kml/nicosia-district-'+ lang +'.kml?t=' + Date.now(),
         larnaca: 'https://raw.githubusercontent.com/yiannisdesp/ucmmap/master/kml/larnaca-district-'+ lang +'.kml?t=' + Date.now(),
-        famagusta: 'https://raw.githubusercontent.com/yiannisdesp/ucmmap/master/kml/ammochostos-district-'+ lang +'.kml?t=' + Date.now(),
+        ammochostos: 'https://raw.githubusercontent.com/yiannisdesp/ucmmap/master/kml/ammochostos-district-'+ lang +'.kml?t=' + Date.now(),
         limassol: 'https://raw.githubusercontent.com/yiannisdesp/ucmmap/master/kml/limassol-district-'+ lang +'.kml?t=' + Date.now(),
         keryneia: 'https://raw.githubusercontent.com/yiannisdesp/ucmmap/master/kml/keryneia-district-'+ lang +'.kml?t=' + Date.now(),
         paphos: 'https://raw.githubusercontent.com/yiannisdesp/ucmmap/master/kml/paphos-district-'+ lang +'.kml?t=' + Date.now(),
     },
-    layers = {};
+    layers = {},
+    districtMarkers = {
+        nicosia: {},
+        larnaca: {},
+        ammochostos: {
+            latlng: {lat: 35.185567, lng: 33.745410},
+            obj: {},
+            path: 'https://raw.githubusercontent.com/yiannisdesp/ucmmap/master/svgs/ammochostos-' + lang + '.svg'
+        },
+        limassol: {},
+        keryneia: {},
+        paphos: {}
+    };
 
 function initMap(){
     map = new google.maps.Map(document.getElementById('map'), {
@@ -16,12 +28,22 @@ function initMap(){
         zoom: 10,
         mapTypeId: 'roadmap'
     });
-    // render layers based on kml data
     for ( k in kmlDataSources ){
+        // render layer
         layers[k] = new google.maps.KmlLayer(kmlDataSources[k], {
             suppressInfoWindows: false,
             preserveViewport: true,
             map: map
+        });
+        // add district text as a marker
+        districtMarkers[k].obj = new google.maps.Marker({
+            position: districtMarkers[k].latlng,
+            map: map,
+            icon: {
+                url: districtMarkers[k].path,
+                size: new google.maps.Size(32, 32),
+                scaledSize: new google.maps.Size(20, 20)
+            }
         });
     }
 }
