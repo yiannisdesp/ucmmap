15ebaae6fbd227018e8d6caf902715e9c4ed779e
Yiannis Despoti <despotis@ucm.org.cy>
2019-08-01 09:04:19 +0300

    -c

--- render.js
+++ render.js
@@ -1,5 +1,5 @@
 var map,
-    lang = 'el',
+    lang = 'en',
     kmlDataSources = {
         nicosia: 'https://raw.githubusercontent.com/yiannisdesp/ucmmap/master/kml/nicosia-district-'+ lang +'.kml',
         larnaca: '',
